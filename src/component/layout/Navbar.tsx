b6ea581f60c07c0b285caa0df27e0ef99d96ccc7
Emilio Torres Jr <emiliotorres3193@gmail.com>
2021-12-09 18:58:42 -0600

    Updated Favicon / Added Logo

--- src/component/layout/Navbar.tsx
+++ src/component/layout/Navbar.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import { ReactComponent as Logo } from "public/logo.svg";
+import { ReactComponent as Logo } from "../../resources/logo/logo.svg";
 
 const Navbar = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Logo/>
+          <Logo height={"5em"}/>
         </Toolbar>
       </AppBar>
     </Box>
