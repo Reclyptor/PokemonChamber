4e13013073d0569e1b6f210c5eba16b6e8538460
Emilio Torres Jr <emiliotorres3193@gmail.com>
2023-05-24 01:50:26 -0500

    Added Animated Background

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -10,7 +10,7 @@ type MainProps = {
 
 const Main = forwardRef<HTMLDivElement, MainProps>((props, ref) => {
   return (
-    <div ref={ ref } className={ clsx("w-screen h-screen overflow-auto", props.className) } style={ props.style }>
+    <div ref={ ref } className={ clsx("w-screen h-screen overflow-auto background-animate bg-gradient-to-r from-background to-primary/[.15]", props.className) } style={ props.style }>
       { props.children }
     </div>
   );
