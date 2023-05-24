531218c5a31f38c81665e41cba5b4ca8ae5afb42
Emilio Torres Jr <emiliotorres3193@gmail.com>
2023-05-24 01:30:27 -0500

    Centered Pokedex Cards

--- src/view/Pokedex.tsx
+++ src/view/Pokedex.tsx
@@ -30,6 +30,7 @@ const Pokedex = (props: PokedexProps) => {
               columnWidth={ cardWidth }
               rowCount={ rowCount }
               columnCount={ colCount }
+              className="flex justify-center"
               cellRenderer={ ({ rowIndex, columnIndex, style }: GridCellProps) => {
                 const pokedexID = rowIndex * colCount + columnIndex + 1;
                 return pokedexID > total ? null : (
