0c95d95f4097ade461ef43f92d19882aff08b57e
Emilio Torres Jr <emiliotorres3193@gmail.com>
2023-05-24 23:28:20 -0500

    Added Debug Mode to Main Layout

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,16 +1,21 @@
 import React, { CSSProperties, forwardRef, ReactNode } from "react";
 import clsx from "clsx";
+import useBreakpoint from "../hook/useBreakpoint";
 import "../tailwind.css";
 
 type MainProps = {
+  debug?: boolean;
   children?: ReactNode;
   className?: string;
   style?: CSSProperties;
 }
 
 const Main = forwardRef<HTMLDivElement, MainProps>((props, ref) => {
+  const { width } = useBreakpoint();
+
   return (
     <div ref={ ref } className={ clsx("w-screen h-screen overflow-auto", props.className) } style={ props.style }>
+      { props.debug && <span className="absolute top-4 right-4 bg-primary-mute rounded-lg px-2 font-bold text-[18px] text-background select-none z-10">{ `${ width }px` }</span> }
       { props.children }
     </div>
   );
